6f6a55a273b5e7bc57c84f5c38d36a79c83fa4ed
uday96055 <uday96055@gmail.com>
2023-05-31 15:17:09 +0530

    addition project

---
+++ src/javaproject/Addition.java
@@ -0,0 +1,13 @@
+package javaproject;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int num1=100;
+		int num2=200;
+		int res=num1+num2;
+		System.out.println(res);
+
+	}
+
+}
