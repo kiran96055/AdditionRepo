86a656071371a3bd565c0a29fba473b4c49cb10d
uday96055 <134064007+uday96055@users.noreply.github.com>
2023-05-31 15:22:31 +0530

    Update Addition.java

--- src/javaproject/Addition.java
+++ src/javaproject/Addition.java
@@ -5,7 +5,8 @@ public class Addition {
 	public static void main(String[] args) {
 		int num1=100;
 		int num2=200;
-		int res=num1+num2;
+		int num3=300;
+		int res=num1+num2+num3;
 		System.out.println(res);
 
 	}
